4e226cafaa3894ed82eb09164c56160e6178aad5
adrianodz <adriano.castan@gmail.com>
2022-05-15 22:59:09 -0300

    create UploadFile model

--- challengebe/models.py
+++ challengebe/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+def path_to_upload(self, filename):
+    extension = filename.split(".")[1]
+    name = (
+        str(timezone.now())
+        .replace("-", "")
+        .replace(":", "")
+        .replace(" ", "-")
+        .split(".")[0]
+    )
+    return f"{name}.{extension}"
+
+
+class UploadFile(models.Model):
+    created_date = models.DateField(verbose_name="Created Date", auto_now=True)
+    uploadfile = models.FileField(
+        verbose_name="Upload File", upload_to=path_to_upload, max_length=500
+    )
